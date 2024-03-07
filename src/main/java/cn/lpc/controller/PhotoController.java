12e0794b7bfe138b0e9bf1f14c990b87ee6725d7
lpc <vbfgde546@163.com>
2024-03-07 22:04:52 +0800

    这是初始版本！！！！

--- src/main/java/cn/lpc/controller/PhotoController.java
+++ src/main/java/cn/lpc/controller/PhotoController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +23,7 @@ import java.util.Date;
  *
  */
 @RequestMapping("/photo")
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class PhotoController {
 
